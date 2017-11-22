eb26fe8de0ca578f9f84e0fd7848dcd5d354950b
Eko Wibowo <eko@monetizemore.com>
2017-11-22 07:19:11 +0700

    bootstrap and jinja

--- web_app/app.py
+++ web_app/app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('settings.py')
+
 
     @app.route('/')
     def index():
-        return 'Halo flask, ini hot reload ya!'
+        return render_template('index.html', TITLE='Flask-01')
 
     @app.route('/about')
     def about():
-        return 'About me'
+        return render_template('about.html', TITLE='Flask-01')
 
     return app
